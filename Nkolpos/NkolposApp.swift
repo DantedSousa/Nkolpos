bd96af012f42482550c2040f18f84a030296ed3c
DantedSousa <danteds.nascimento@gmail.com>
2025-12-05 14:50:15 -0300

    Initial Commit

---
+++ Nkolpos/NkolposApp.swift
@@ -0,0 +1,17 @@
+//
+//  NkolposApp.swift
+//  Nkolpos
+//
+//  Created by found on 05/12/25.
+//
+
+import SwiftUI
+
+@main
+struct NkolposApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
